431e2cc3af01d6df305c2eb4eeca676c1311a675
ericzhao <pengfei19860421@gmail.com>
2013-08-21 16:38:11 +0800

    add more function

--- OpenriceCrawler/src/foodReview2013/HelloWorld.java
+++ OpenriceCrawler/src/foodReview2013/HelloWorld.java
@@ -5,9 +5,15 @@ public class HelloWorld {
 	public String advanceFunction(){
 		return "add some function";
 	}
+	
+	public String addMoreFunction(){
+		return "more function";
+	}
+	
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		HelloWorld hello = new HelloWorld();
 		String a = hello.advanceFunction();
+		String b = hello.addMoreFunction();
 	}
 }

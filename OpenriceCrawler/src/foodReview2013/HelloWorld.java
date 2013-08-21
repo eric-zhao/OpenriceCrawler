d47363fe1bd682041c6599ca29855c232af9fcb1
ericzhao <pengfei19860421@gmail.com>
2013-08-21 16:05:12 +0800

    add a function named as "advance function"

--- OpenriceCrawler/src/foodReview2013/HelloWorld.java
+++ OpenriceCrawler/src/foodReview2013/HelloWorld.java
@@ -1,7 +1,13 @@
 package foodReview2013;
 
 public class HelloWorld {
+	
+	public String advanceFunction(){
+		return "add some function";
+	}
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+		HelloWorld hello = new HelloWorld();
+		String a = hello.advanceFunction();
 	}
 }
